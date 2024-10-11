ea75110ac8c95fd0d10bd39e971cf7aff57836fb
cjtosh <ctosh@cs.ucsd.edu>
2024-10-11 16:11:54 -0400

    increase time

--- scripts/nonadditive.sh
+++ scripts/nonadditive.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 #SBATCH --array=0-99                                                                                              
 #SBATCH --output=output_logs/nonadd_%a.out  
 #SBATCH --error=output_logs/nonadd_%a.err  
